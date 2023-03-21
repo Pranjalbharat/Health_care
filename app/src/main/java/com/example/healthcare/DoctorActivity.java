9401cb1d26bcf3c4af835e1609878d408a032a49
Shubham Puhal <shubhampuhal01@gmail.com>
2023-03-22 00:46:55 +0530

    Improved Input credential error of login pages.

--- app/src/main/java/com/example/healthcare/DoctorActivity.java
+++ app/src/main/java/com/example/healthcare/DoctorActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,10 +45,17 @@ public class DoctorActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String text_email=email.getText().toString();
                 String text_password=password.getText().toString();
-                if(TextUtils.isEmpty(text_email) || TextUtils.isEmpty(text_password)){
-                    Toast.makeText(DoctorActivity.this,"Empty credentials",Toast.LENGTH_LONG).show();
-                }else if(text_password.length() <6){
-                    Toast.makeText(DoctorActivity.this,"password is too short",Toast.LENGTH_LONG).show();
+                if(TextUtils.isEmpty(text_email)){
+                    email.setError("Empty Credentials");
+                }
+                if(TextUtils.isEmpty(text_password)){
+                    password.setError("Empty Credentials");
+                }
+                else if(!isEmail(email)){
+                    email.setError("Email address is not correct.");
+                }
+                else if(text_password.length() <6){
+                    password.setError("password too short");
                 }else{
                     registerUser(text_email,text_password);
                 }
@@ -60,6 +68,11 @@ public class DoctorActivity extends AppCompatActivity {
 
 
 
+    }
+
+    boolean isEmail(EditText text) {
+        String email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
     private void registerUser(String email, String password) {
@@ -75,7 +88,6 @@ public class DoctorActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
 
--- app/src/main/java/com/example/healthcare/UserActivity.java
+++ app/src/main/java/com/example/healthcare/UserActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,20 +44,31 @@ public class UserActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String text_email=email.getText().toString();
                 String text_password=password.getText().toString();
-                if(TextUtils.isEmpty(text_email) || TextUtils.isEmpty(text_password)){
-                    Toast.makeText(UserActivity.this,"Empty credentials",Toast.LENGTH_LONG).show();
-                }else if(text_password.length() <6){
-                    Toast.makeText(UserActivity.this,"password is too short",Toast.LENGTH_LONG).show();
+                if(TextUtils.isEmpty(text_email)){
+                    email.setError("Empty Credentials");
+                }
+                if(TextUtils.isEmpty(text_password)){
+                    password.setError("Empty Credentials");
+                }
+                else if(!isEmail(email)){
+                    email.setError("Email address is not correct.");
+                }
+                else if(text_password.length() <6){
+                    password.setError("password too short");
                 }else{
                     registerUser(text_email,text_password);
                 }
-
             }
         });
 
 
 
 
+    }
+
+    boolean isEmail(EditText text) {
+        String email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
     private void registerUser(String email, String password) {
