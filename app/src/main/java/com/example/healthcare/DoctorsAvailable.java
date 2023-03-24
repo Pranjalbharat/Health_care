592c49365fbd2f63e633130672b6f35800d0f78b
Pranjal <pranjalbharat07@gmail.com>
2023-03-24 13:22:54 +0530

    Applied OnClick on ImageButton in Available Doctors

--- app/src/main/java/com/example/healthcare/DoctorsAvailable.java
+++ app/src/main/java/com/example/healthcare/DoctorsAvailable.java
@@ -2,7 +2,10 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DoctorsAvailable extends AppCompatActivity {
 
@@ -10,5 +13,143 @@ public class DoctorsAvailable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctors_available);
+
+        ImageButton Button1=findViewById(R.id.imageButton);
+        ImageButton Button2=findViewById(R.id.imageButton2);
+        ImageButton Button3=findViewById(R.id.imageButton3);
+        ImageButton Button4=findViewById(R.id.imageButton4);
+        ImageButton Button5=findViewById(R.id.imageButton5);
+        ImageButton Button6=findViewById(R.id.imageButton6);
+        ImageButton Button7=findViewById(R.id.imageButton7);
+        ImageButton Button8=findViewById(R.id.imageButton8);
+        ImageButton Button9=findViewById(R.id.imageButton9);
+        ImageButton Button10=findViewById(R.id.imageButton10);
+
+        Button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Shubham";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Pranjal ";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Rashmi ";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Shreya";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Swayam";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Neeraj";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Vanshika";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Ditya";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Pranshu";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        Button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str="Sucrose";
+                Intent intent = new Intent(getApplicationContext(), profile_doctors.class);
+                intent.putExtra("data", str);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
